0ac01ddd8b10e1621ff3e9f592017d40d7387508
Adib Zefar <adibzefar@gmail.com>
2022-05-19 14:11:31 -0400

    Remove unused code from the module

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,10 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  // $('.submit-btn').on('click', function (event) {
-  //   event.preventDefault();
-  //   alert("fire");
-  // })
 
   $('#tweet-text').on('keydown', function () {
     let $words = $(this).val().length;
     const $counter = $('output.counter');
     let $totalWords = 140 - $words;
     console.log("words:", $words);
-    // console.log($(this));
-    // console.log("counter:", counter);
-    // counter -= words;
     $counter.text($totalWords);
     
     if ($words > 140) {
