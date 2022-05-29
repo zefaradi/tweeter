271a13be4b9fc83d568d63cb1b798be8a45d8e5e
Adib Zefar <adibzefar@gmail.com>
2022-05-28 23:23:24 -0400

    eslint the code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-  $('#tweet-text').on('input', function () {
+  $('#tweet-text').on('input', function() {
     let $words = $(this).val().length;
     const $counter = $('output.counter');
     let $totalWords = 140 - $words;
     $counter.text($totalWords);
     
     if ($words > 140) {
-      $counter.addClass("error")
+      $counter.addClass("error");
     } else {
-      $counter.removeClass("error")
+      $counter.removeClass("error");
     }
-  })
+  });
 });
